4be9b9954d3fcd55a0fc5efbd5106f31497ca3c3
Uchenna99 <uchennagbu@gmail.com>
2024-10-21 22:01:40 +0100

    some touches

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -7,10 +7,10 @@ export const Form =()=>{
         <>
             <div className="form-body">
                 <form >
-                    <input type="text" />
-                    <input type="text" />
-                    <input type="text" />
-                    <input type="text" />
+                    <input type="text" placeholder="First Name" />
+                    <input type="text" placeholder="Last Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
                     <button>Submit Form</button>
                 </form>
             </div>
