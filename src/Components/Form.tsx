74b9e3f67e8de7401ea6851cd4d0d930a13e316c
Uchenna99 <uchennaagbu@gmail.com>
2024-12-21 13:15:53 +0100

    Remaking the form to consume api

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
-import { FieldValues, useForm } from "react-hook-form";
-import { FormData } from "./FormObject";
+import { FormData, UserData } from "./FormObject";
+import axios from "axios";
+
 
 
 export const Form =()=>{
+    const [users, setUsers] = useState<UserData[]>([]);
+    const [forminfo, setForminfo] = useState<FormData>({
+        firstName:'',
+        lastName:'',
+        email:'',
+        password:''
+    });
+
 
     const [visibility, setVisibility] = useState(false)
 
@@ -13,50 +22,43 @@ export const Form =()=>{
     }
 
 
-    const {register, handleSubmit, formState: {errors, isValid}} = useForm<FormData>(); 
-     
-      
-
-    const onSubmit =(data: FieldValues)=>{
-        console.log(data)
-    }
+    const onSubmit = async () => {
+        try {
+          const response = await axios.post('http://localhost:3010/api/v1/users', forminfo);
+          console.log(response.data);
+        } catch (error: any) {
+          if (axios.isAxiosError(error)) {
+            console.error('Axios Error:', error.response?.data);
+          } else {
+            console.error('Unexpected Error:', error);
+          }
+        }
+      };
 
     return (
         <>
-            <div className="form-body">
-                <form onSubmit={handleSubmit(onSubmit)} >
-                    <div className="input-wrap">
-                        <input type="text" placeholder="First Name" {...register('firstName', {required: true, minLength: 3})} />
-                        { errors.firstName?.type === 'required' && <p id="err-p">This input is required.</p> }
-                        { errors.firstName?.type === 'minLength' && <p id="err-p">The name must be at least 3 characters long.</p> }
-                    </div>
-                    <div className="input-wrap">
-                        <input type="text" placeholder="Last Name" {...register('lastName', {required: true, minLength: 3})} />
-                        {errors.lastName?.type === 'required' && <p id="err-p">This input is required.</p> }
-                        {errors.lastName?.type === 'minLength' && <p id="err-p">The name must be at least 3 characters long.</p>}
-                    </div>
-                    <div className="input-wrap">
-                        <input type="email" placeholder="Email" {...register('email', {required: true, minLength: 6})} />
-                        {errors.email?.type === 'required' && <p id="err-p">This input is required.</p> }
-                    </div>
-                    
-                    <div className="input-wrap">
-                        <div className="input-wrap">
-                            <input type={visibility == false? "password" : "text"} 
-                            {...register('password', {required: true, minLength: 8, maxLength: 25})}
-                            placeholder="Password" />
-                            {errors.password?.type === 'required' && <p id="err-p">This input is required.</p> }
-                            {errors.password?.type === 'minLength' && <p id="err-p">The password must be at least 8 characters long.</p> }
-                            {errors.password?.type === 'maxLength' && <p id="err-p">Password must not be longer than 25 characters.</p>}
-                            <FaRegEye id="eye" onClick={visibilityToggle} style={{display: visibility == true? 'none' : 'flex'}} />
-                            <FaRegEyeSlash id="eye" onClick={visibilityToggle} style={{display: visibility == true? 'flex' : 'none'}} />
-                        </div>
-                        
-                        
-                    </div>
+            <div className="page-wrap">
+                <div className="add-user-section">
+                    <div className="form">
+                        <input type="text" placeholder="First Name"/>
+                        <input type="text" placeholder="Last Name"/>
+                        <input type="email" placeholder="E-mail"/>
+                        <input type="password" placeholder="Password"/>
 
-                    <button disabled={!isValid} type="submit" >Submit Form</button>
-                </form>
+                        <button>Create User</button>
+                    </div>
+                </div>
+
+                <div className="users-display-section">
+                    <div className="user-grid">
+                        <div className="grid-header"> <h3>ID</h3> </div>
+                        <div className="grid-header"> <h3>FIRST NAME</h3> </div>
+                        <div className="grid-header"> <h3>LAST NAME</h3> </div>
+                        <div className="grid-header"> <h3>E-MAIL</h3> </div>
+
+                       
+                    </div>
+                </div>
             </div>
         </>
     )
