b94a1f0e1c692c60d112ef35e8d24d8c4389ec0e
Uchenna99 <uchennaagbu@gmail.com>
2025-01-18 12:02:59 +0100

    setup login form axios

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,13 +1,32 @@
 import { useState } from "react";
 import { LoginFormData } from "./FormObject";
+import axios from "axios";
+import { login_url } from "../URL";
 
 interface Props{
     show: ()=>void;
 }
 
+interface Tokens{
+    accessToken: string;
+    refreshToken: string;
+}
+
 const Login = ({ show }:Props) => {
-    const baseForm: LoginFormData = { email:'', password:'' }
+    const [tokens, setTokens] = useState<Tokens | null>(null);
+    const baseForm: LoginFormData = { email:'', password:'' };
     const [forminfo, setForminfo] = useState<LoginFormData>(baseForm);
+
+    const handleLogin = async ()=>{        
+        try {
+            await axios.post(login_url, forminfo)
+            .then(res => {setTokens(res.data)})
+            
+        } catch (error) {
+            console.error('login failed:', error);
+        }
+        
+    };
     
   return (
     <>
@@ -15,13 +34,13 @@ const Login = ({ show }:Props) => {
 
             <input type="email" placeholder="E-mail"
             value={forminfo.email} 
-            onChange={(e)=> setForminfo({...forminfo, email: e.target.value})}/>
+            onChange={(e)=> { setForminfo({...forminfo, email: e.target.value})}}/>
             
             <input type="password" placeholder="Password"
             value={forminfo.password} 
             onChange={(e)=> setForminfo({...forminfo, password: e.target.value})}/>
 
-            <button >Login</button>
+            <button onClick={handleLogin} >Login</button>
 
             <p>Don't have an account? <span onClick={show}>Signup</span> </p>
         </div>
--- src/URL.ts
+++ src/URL.ts
@@ -1,2 +1,4 @@
 
 export const base_users_url = 'http://localhost:3010/api/v1/users';
+
+export const login_url = 'http://localhost:3010/api/v1/login' 
